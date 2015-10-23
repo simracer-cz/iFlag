3536a8746ea2324a15df0293e31db6aab135741c
Petr Vostřel <petr@vostrel.cz>
2015-10-23 20:48:22 +0200

    Options button added

--- software-source/iFlag/FormMain.Designer.cs
+++ software-source/iFlag/FormMain.Designer.cs
@@ -29,11 +29,28 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.optionsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // optionsButton
+            // 
+            this.optionsButton.Location = new System.Drawing.Point(155, 108);
+            this.optionsButton.Name = "optionsButton";
+            this.optionsButton.Size = new System.Drawing.Size(59, 23);
+            this.optionsButton.TabIndex = 1;
+            this.optionsButton.Text = "Options";
+            this.optionsButton.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.optionsButton);
             this.Text = "FormMain";
         }
 
         #endregion
+
+        private System.Windows.Forms.Button optionsButton;
     }
 }
 
