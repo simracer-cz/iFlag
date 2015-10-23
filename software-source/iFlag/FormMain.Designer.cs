2a6dd8acd7a92c49a1a80d3fa0f8d807c3c25614
Petr Vostřel <petr@vostrel.cz>
2015-10-23 20:50:51 +0200

    Some other VS-generated stuff

--- software-source/iFlag/FormMain.Designer.cs
+++ software-source/iFlag/FormMain.Designer.cs
@@ -43,9 +43,14 @@
             // 
             // FormMain
             // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.optionsButton);
+            this.Name = "FormMain";
             this.Text = "FormMain";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
