c6850b98e0efb569a7936af2cbc19ee3b278af81
Petr Vostřel <petr@vostrel.cz>
2015-10-23 23:17:14 +0200

    Haha you wish!

--- software-source/iFlag/Properties/AssemblyInfo.cs
+++ software-source/iFlag/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("iFlag")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Light flags for iRacing")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("iFlag")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("© 2015 Petr Vostrel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
