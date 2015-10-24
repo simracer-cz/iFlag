74142e223ab9f27999db96a8bf48eb1a7b3237f2
Petr Vostřel <petr@vostrel.cz>
2015-10-24 02:10:26 +0200

    iRacing SDK added

---
+++ software-source/iFlag/sdk.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+using iRSDKSharp;
+
+namespace iFlag
+{
+    public partial class mainForm : Form
+    {
+        iRacingSDK sdk = new iRacingSDK();
+        
+        private void startSDK()
+        {
+        }
+    }
+}
