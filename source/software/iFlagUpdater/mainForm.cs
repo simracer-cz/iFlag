336da66f6eb4ef1c9db4dfb79cc3dc8c8e3f2941
Petr Vostřel <petr@vostrel.cz>
2016-04-24 19:38:00 +0200

    Download all files to update, one by one and just sit them aside, don't overwrite unless the download is 100% complete

--- source/software/iFlagUpdater/mainForm.cs
+++ source/software/iFlagUpdater/mainForm.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Drawing;
+using System.Net;
 using System.Windows.Forms;
 
 namespace iFlagUpdater
 {
     public partial class mainForm : Form
     {
+        string appFilename = "iFlag.exe";
         int[] windowLocation = { 0, 0 };
         string version = "";
         string updateVersion = "";
+
+        string updateRepository = "https://github.com/simracer-cz/iFlag";
+        string updateRepositoryFormat = "{0}/blob/{1}/software/iFlag/{2}?raw=true";
+        string updateDownloadFormat = "{0}.update";
+        string[] updateFiles = {
+          "iFlag.exe",
+          "iFlag.exe.config",
+          "iFlag.pdb",
+          "device/firmware.hex",
+        };
+        int updatedCount;
 
         public mainForm(string[] args)
         {
@@ -25,6 +38,7 @@ namespace iFlagUpdater
             windowLocation[1] = Convert.ToInt32(args[3]);
 
             InitializeComponent();
+            performUpdate();
         }
 
                                                   // When the window opens,
@@ -38,6 +52,44 @@ namespace iFlagUpdater
 
             beforeLabel.Text = version;
             afterLabel.Text = updateVersion;
+        }
+
+                                                  // Gets called repeatedly for each update files list entry.
+                                                  // It downloads new versions of files and stores then
+                                                  // as *.update files while keeping count.
+        private void performUpdate(){
+            if (updatedCount < updateFiles.Length)
+            {
+                try
+                {
+                    string filename = updateFiles[updatedCount];
+                    updatedCount++;
+                    string url = string.Format(updateRepositoryFormat, updateRepository, updateVersion, filename);
+                    string file = string.Format(updateDownloadFormat, filename);
+
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(downloadComplete);
+                    webClient.DownloadFileAsync(new Uri(url), @file);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Sorry, update download failed.");
+                    returnToAppNow();
+                }
+
+        private void downloadComplete(object sender, AsyncCompletedEventArgs e)
+        {
+            performUpdate();
+        }
+
+        private void returnToAppNow()
+        {
+            Process process = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = appFilename;
+            process.StartInfo = info;
+            process.Start();
+            Application.Exit();
         }
     }
 }
