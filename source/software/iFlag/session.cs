4ed5bf9f6105943db2b0da30e7d35d53dcfb3538
Petr Vostřel <petr@vostrel.cz>
2019-03-05 02:43:58 +0100

    Reading more session info

--- source/software/iFlag/session.cs
+++ source/software/iFlag/session.cs
@@ -8,7 +8,9 @@ namespace iFlag
     public partial class mainForm : Form
     {
         bool sessionDetected;
+        string sessionInfo;
         int carID;
+        int paceCarID;
         string trackCategory;
 
         private void startSession()
@@ -22,15 +24,27 @@ namespace iFlag
         {
             if (!sessionDetected)
             {
-                string sessionInfo = sdk.GetSessionInfo();
+                sessionInfo = sdk.GetSessionInfo();
 
                 if (sessionInfo != "")
                 {
                     sessionDetected = true;
 
+                    Match sessionIDMatch = Regex.Match(sessionInfo, @"(?<=SessionID: )\d+");
+                    int sessionID = int.Parse(sessionIDMatch.Value);
+                    Console.WriteLine("SessionID: {0}", sessionID);
+
+                    Match subSessionIDMatch = Regex.Match(sessionInfo, @"(?<=SubSessionID: )\d+");
+                    int subSessionID = int.Parse(subSessionIDMatch.Value);
+                    Console.WriteLine("SubSessionID: {0}", subSessionID);
+
                     Match carIdxMatch = Regex.Match(sessionInfo, @"(?<=DriverCarIdx: )\d+");
                     carID = int.Parse(carIdxMatch.Value);
                     Console.WriteLine("Car ID: {0}", carID);
+
+                    Match paceCarIdxMatch = Regex.Match(sessionInfo, @"(?<=PaceCarIdx: )[-0-9.]+");
+                    paceCarID = int.Parse(paceCarIdxMatch.Value);
+                    Console.WriteLine("Pace car ID: {0}", paceCarID);
 
                     Match category = Regex.Match(sessionInfo, @"(?<=Category: )(Road|Oval)");
                     trackCategory = category.Value;
