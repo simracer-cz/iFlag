90d64289c7690067d419e17ab295ab7ebc9a4913
Petr Vostřel <petr@vostrel.cz>
2020-03-10 00:35:01 +0100

    Spotter module enabled by default

--- source/software/iFlag/Properties/Settings.Designer.cs
+++ source/software/iFlag/Properties/Settings.Designer.cs
@@ -97,7 +97,7 @@ namespace iFlag.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool ShowSpotterOverlay {
             get {
                 return ((bool)(this["ShowSpotterOverlay"]));
