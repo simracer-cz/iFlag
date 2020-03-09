8b0f8c6b764c8ae1c4f0bc90078fc5e9070d4a20
Petr Vostřel <petr@vostrel.cz>
2020-03-09 16:22:12 +0100

    New window for "virtual" on-screen matrix

---
+++ source/software/iFlag/VirtualMatrix.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace iFlag
+{
+    partial class VirtualMatrix
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // VirtualMatrix
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(240, 240);
+            this.ContextMenuStrip = this.contextMenuStrip;
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "VirtualMatrix";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "iFLAG Virtual Matrix";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ source/software/iFlag/VirtualMatrix.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+
+using iFlag.Properties;
+
+namespace iFlag
+{
+    public partial class VirtualMatrix : Form
+    {
+        public VirtualMatrix()
+        {
+            InitializeComponent();
+        }
+    }
+}
