1e372235f4fade2b4852ff8572c0521335c204ae
Petr Vostřel <petr@vostrel.cz>
2020-03-10 18:16:53 +0100

    Year update

--- source/firmware/iFlag/iFlag.ino
+++ source/firmware/iFlag/iFlag.ino
@@ -13,7 +13,7 @@
 //   2. Colorduino Library for Arduino IDE (https://github.com/lincomatic/Colorduino)
 //   3. iFLAG host software (https://github.com/simracer-cz/iFlag)
 //
-// (c) 2015-2019 Petr.Vostrel.cz
+// (c) 2015-2020 Petr.Vostrel.cz
 
 #include <Colorduino.h>
 #include <EEPROM.h>
