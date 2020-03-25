f1bae0aae7551ba7ec6d40ed4b6f46d2864209c3
Petr Vostřel <petr@vostrel.cz>
2020-03-25 01:18:40 +0100

    v0.89 release, added: Suppression of in-pit signals after the checkered

--- source/software/iFlag/mainForm.cs
+++ source/software/iFlag/mainForm.cs
@@ -11,7 +11,7 @@ namespace iFlag
     public partial class mainForm : Form
     {
         // Version
-        const string version = "v0.88";
+        const string version = "v0.89";
         const bool updatable = true;
 
         // Embedded firmware version
